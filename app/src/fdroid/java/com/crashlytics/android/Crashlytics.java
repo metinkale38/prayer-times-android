ccfadee66316656703b83a68f450acce83dbcd78
metinkale38 <metinkale38@gmail.com>
2016-08-08 15:58:43 +0200

    added FDroid flavour without Crashlytics

---
+++ app/src/fdroid/java/com/crashlytics/android/Crashlytics.java
@@ -0,0 +1,21 @@
+package com.crashlytics.android;
+
+import java.io.IOException;
+
+/**
+ * Created by metin on 08.08.16.
+ */
+public class Crashlytics {
+    public static void logException(Exception e) {
+
+    }
+
+    public static void setDouble(String key, double value) {
+    }
+
+    public static void setString(String key, String value) {
+    }
+
+    public static void setBool(String key, boolean value) {
+    }
+}
---
+++ app/src/fdroid/java/io/fabric/sdk/android/Fabric.java
@@ -0,0 +1,12 @@
+package io.fabric.sdk.android;
+
+import android.content.Context;
+import com.crashlytics.android.Crashlytics;
+
+/**
+ * Created by metin on 08.08.16.
+ */
+public class Fabric {
+    public static void with(Context c, Crashlytics crashlytics) {
+    }
+}
