f0cec07cdf98e0540412cad01f5a24f898873dc4
metinkale38 <metinkale38@gmail.com>
2020-04-19 01:19:32 +0200

    FIXED fdroidPrep.sh

--- fdroidPrep.sh
+++ fdroidPrep.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sed -i '/excludeInFDroid/s/^/\/\//' app/build.gradle
+find . | grep build.gradle | xargs sed -i '/excludeInFDroid/s/^/\/\//'
+echo "android.enableJetifier=true" >> gradle.properties
+echo "android.useAndroidX=true" >> gradle.properties
+echo "org.gradle.configureondemand=true" >> gradle.properties
+echo "org.gradle.jvmargs=-Xmx3g -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8" >> gradle.properties
+echo "org.gradle.caching=true" >> gradle.properties
+echo "org.gradle.daemon=true" >> gradle.properties
+echo "org.gradle.parallel=true" >> gradle.properties
